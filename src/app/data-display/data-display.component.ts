32a34312d5c77f613ca07680dd323fa3480d0107
deekay1938 <deekay1938@gmail.com>
2025-09-22 14:37:19 +0000

    UI changes

--- src/app/data-display/data-display.component.ts
+++ src/app/data-display/data-display.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
 import { faFilm,faCloud, faThermometerEmpty, faWater, faArrowCircleDown} from '@fortawesome/free-solid-svg-icons';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-data-display',
   templateUrl: './data-display.component.html',
-  styleUrls: ['./data-display.component.css']
+  styleUrls: ['./data-display.component.css'],
+  animations: [
+    trigger('popOut', [
+      transition(':enter', [
+        style({ transform: 'scale(0) translateY(-100%)', opacity: 0 }),
+        animate('800ms ease-out', style({ transform: 'scale(1) translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('800ms ease-in', style({ transform: 'scale(0) translateY(-100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class DataDisplayComponent implements OnInit{
   weatherData:any;
@@ -18,14 +30,14 @@ export class DataDisplayComponent implements OnInit{
   forcastData: any;
   apiIcon:any;
   iconLInk: string | undefined;
-  
-  // Angular Icons 
+
+  // Angular Icons
   filmIcon = faFilm;
   cloudIcon = faCloud
   thermoIcon = faThermometerEmpty
   humidityIcon = faWater
   pressureIcon = faArrowCircleDown
-  
+
 
   constructor(private weatherService:WeatherService){
 
@@ -56,7 +68,7 @@ export class DataDisplayComponent implements OnInit{
     onCelsius(){
       this.isCelsius = true;
       this.isFahrenheit = false;
-      
+
      }
 
     onFahrenheit(){
@@ -67,14 +79,13 @@ export class DataDisplayComponent implements OnInit{
      getWeather(){
       this.weatherService.getCityWeather(this.location).subscribe((res) => {
         this.weatherData = res;
+        this.apiIcon =this.weatherData.weather[0].icon;
+        this.iconLInk = "http://openweathermap.org/img/w/"+this.apiIcon+".png";
         console.log(res)}, error=>{
           console.log(error);
           this.displayError = error;
           this.ifError = true;
         });
-         this.apiIcon =this.weatherData.weather[0].icon
-         this.iconLInk = "http://openweathermap.org/img/w/"+this.apiIcon+".png"
-         
     }
 
     closeWeather(){
